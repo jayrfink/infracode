87c6649bbc65a584797392ae28d693a4aaf57c71
jayrfink <jay.fink@gmail.com>
2020-10-21 10:23:18 -0400

    Added policies and more in depth s3 examples.

--- sysnet/aws-adrift-us-east-1/s3_storage.tf
+++ sysnet/aws-adrift-us-east-1/s3_storage.tf
@@ -9,5 +9,47 @@ resource "aws_s3_bucket" "adrift-example-bucket" {
     Name = "doomwad-repo"
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+
+    lifecycle_rule {
+    id      = "adrift-id-23456"
+    enabled = true
+
+    prefix = "optional/"
+
+    tags = {
+      "rule"      = "adrift-s3-rule"
+      "autoclean" = "true"
+    }
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA" # or "ONEZONE_IA"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 90
+    }
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "adrift-example-bucket" {
+  bucket                  = aws_s3_bucket.adrift-example-bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
--- sysnet/aws-iam/groups.tf
+++ sysnet/aws-iam/groups.tf
@@ -1,3 +1,17 @@
+resource "aws_iam_user" "jrf" {
+  name = "jrf"
+
+  tags = {
+    Name = "Jay_Fink"
+    Dept = "EOD"
+  }
+}
+
+resource "aws_iam_access_key" "jrf" {
+  user = aws_iam_user.jrf.name
+}
+
+
 resource "aws_iam_group_membership" "syssec" {
   name = "syssec"
   group = "syssec"
@@ -32,3 +46,56 @@ resource "aws_iam_group_membership" "netsec" {
     "jill",
    ]
 }
+
+resource "aws_iam_group_policy" "systor-s3-policy" {
+  name = "systor-s3-policy"
+  group = aws_iam_group.systor.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:s3:::somebucket",
+        "arn:aws:s3:::somebucket/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_group_policy" "sysadm-ec2-policy" {
+  name = "sysadm-ec2-policy"
+  group = aws_iam_group.sysadm.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+         "Effect":"Allow",
+         "Action":"ec2:Describe*",
+         "Resource":"*"
+      },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:StartInstances",
+            "ec2:StopInstances",
+            "ec2:RebootInstances"
+         ],
+         "Resource":[
+            "arn:aws:ec2:us-east-1:111122223333:instance/*"
+         ]
+    }
+  ]
+}
+EOF
+}
+
