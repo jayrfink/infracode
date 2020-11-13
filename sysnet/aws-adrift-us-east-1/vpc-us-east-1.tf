9dd43ef0a424e203fd92f1e295ea542674679ccd
jayrfink <jay.fink@gmail.com>
2020-11-13 11:45:13 -0500

    Enabled DNS in VPC.

--- sysnet/aws-adrift-us-east-1/vpc-us-east-1.tf
+++ sysnet/aws-adrift-us-east-1/vpc-us-east-1.tf
@@ -2,6 +2,9 @@ resource "aws_vpc" "vpc-adrift" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
+  enable_dns_support = "true"                                                   
+  enable_dns_hostnames = "true"
+
   tags = {
     Name = "vpc-adrift"
   }
